f3ee22d0194fd046ad11c6fa18ba92055b54b579
Gary Oberbrunner <garyo@darkstarsystems.com>
2025-06-03 10:13:11 -0400

    Add robots.txt, meta-description & sitemap using @astrojs/sitemap
    Signed-off-by: Gary Oberbrunner <garyo@darkstarsystems.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  site: 'https://deep-timeline.oberbrunner.com',
+  integrations: [solidJs(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
